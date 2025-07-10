ab1df10e1483431b11d0453cc0fa58f4a3f4200d
Yug Sutariya <yugsutariya@yugs-air.home>
2025-07-10 14:12:51 -0400

    updated terraform provider

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"  # or higher like "~> 4.0"
+    }
+  }
+}
+
 provider "azurerm" {
   features {}
 }
